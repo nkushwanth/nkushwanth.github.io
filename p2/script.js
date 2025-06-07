cb6bc38ded6199c0006e96930e842daaeea3f532
nkushwanth <nagamkushwanthreddy@gmail.com>
2025-06-07 11:09:38 +0530

    updated

--- p2/script.js
+++ p2/script.js
@@ -18,12 +18,12 @@ const showRegister = () => {
     <p><input type="text" id="txtName"></p>
      <p><input type="text" id="txtEmail"></p>
     <p><input type="password" id="txtPass"></p>
-    <button>Register</button>
-    <button>aldready a member</button>
+    <button onclick ='addUser()'>Register</button>
+    <button onclick ='showLogin()'>aldready a member</button>
     `
     root.innerHTML = str
 }
-
+              
 const showhomw = () =>{
     let str= `<h1>Register Form</h1>`
 
